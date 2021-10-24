3425bf0047dc1e025a56fe5b2fdc715d27857c13
Eugeniusz W <eugwas@gmail.com>
2021-10-24 17:15:07 +0200

    First commit

---
+++ src/com/company/PerfectNumber.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                int divisor = i;
+                System.out.println("Divisor = " + divisor);
+                sum += divisor;
+            }
+        }
+        System.out.println("Sum = " + sum);
+        if (number != sum) {
+            return false;
+        }
+
+        return true;
+    }
+}
