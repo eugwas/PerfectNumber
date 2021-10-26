6bd4415da9d9f653567f2448fe396765b2c01085
Eugeniusz W <eugwas@gmail.com>
2021-10-26 18:03:49 +0200

    Scanner was added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(PerfectNumber.isPerfectNumber(28));
+        System.out.println("Perfect number is a positive integer that is equal to the sum of its positive divisors.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        boolean hasNextInt = scanner.hasNextInt();
+        if (hasNextInt) {
+            int number = scanner.nextInt();
+            System.out.println("Is perfect number = " + PerfectNumber.isPerfectNumber(number));
+        } else {
+            System.out.println("Invalid number");
+        }
+        scanner.nextLine();
+        scanner.close();
     }
 }
